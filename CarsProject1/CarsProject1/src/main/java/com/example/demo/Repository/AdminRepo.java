bed197769c6c15e5c97b8c144d069830fd8cefb0
mani1688 <85572394+mani1688@users.noreply.github.com>
2023-11-27 12:30:07 -0600

    Delete CarsProject1/CarsProject1 directory

--- CarsProject1/CarsProject1/src/main/java/com/example/demo/Repository/AdminRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.Admin;
-
-public interface AdminRepo extends JpaRepository<Admin, String> {
-
-}
--- CarsProject1/CarsProject1/src/main/java/com/example/demo/Repository/BankTransactionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.BankTransaction;
-
-public interface BankTransactionRepo  extends JpaRepository<BankTransaction, Long>{
-
-}
